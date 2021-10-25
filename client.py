c0e0ce9c1682afd30f8d03b564d29b962afd50af
leviben2 <benlevi7770528@gmail.com>
2021-10-25 09:27:13 -0700

    client

--- client.py
+++ client.py
@@ -9,24 +9,19 @@ file_name = sys.argv[3]
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.settimeout(12)
 
-with open(file_name) as file:
-    data = file.read()
-    arr = [data[i:i + 97] for i in range(0, len(data), 97)]
-    for i in range(0, len(arr)):
-        if i < 10:
-            arr[i] += '00' + str(i)
-        elif i < 100:
-            arr[i] += '0' + str(i)
-        else:
-            arr[i] += str(i)
+text = open(file_name, "rb")
+data = text.read()
+arr = [data[i:i + 97] for i in range(0, len(data), 97)]
+for i in range(0, len(arr)):
+    arr[i] += i.to_bytes(3, 'little')
 
 ack_arr = [False for i in range(len(arr))]
 
 
 def start_net(num_pak):
-    s.sendto(str(num_pak).encode('utf8'), (ip, port))
+    s.sendto(num_pak, (ip, port))
     try:
-        data, addr = s.recvfrom(3)
+        get_num, addr = s.recvfrom(3)
         send_paks()
     except socket.timeout:
         start_net(num_pak)
@@ -46,8 +41,8 @@ def ack():
     try:
         while True:
             data_ack, addr = s.recvfrom(100)
-            pck_num = str(data_ack)[-3:len(str(data_ack))]
-            ack_arr[int(pck_num)] = True
+            pck_num = int.from_bytes(data_ack[-3:len(data_ack)], 'little')
+            ack_arr[pck_num] = True
     except:
         if not finish():
             send_paks()
@@ -60,5 +55,5 @@ def finish():
     return True
 
 
-start_net((len(arr)))
+start_net((len(arr)).to_bytes(3, 'little'))
 s.close()
