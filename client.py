0e3760cbd77c3f27982f1c0cfaff76a4ac4abcdb
leviben2 <benlevi7770528@gmail.com>
2021-10-25 08:00:39 -0700

    client & server

--- client.py
+++ client.py
@@ -3,51 +3,55 @@ import sys
 import time
 
 ip = sys.argv[1]
-port = sys.argv[2]
+port = int(sys.argv[2])
 file_name = sys.argv[3]
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.settimeout(12)
 
-with open(file_name) as file :
+with open(file_name) as file:
     data = file.read()
-    arr = [data[i:i + 96] for i in range(0, len(data), 96)]
+    arr = [data[i:i + 97] for i in range(0, len(data), 97)]
     for i in range(0, len(arr)):
-        if (i < 10):
+        if i < 10:
             arr[i] += '00' + str(i)
-        elif (i < 100):
+        elif i < 100:
             arr[i] += '0' + str(i)
         else:
             arr[i] += str(i)
 
-ack_arr =  [False for i in range(len(arr))]
+ack_arr = [False for i in range(len(arr))]
+
 
 def start_net(num_pak):
-    s.sendto(bytes(num_pak), (ip, port))
+    s.sendto(str(num_pak).encode('utf8'), (ip, port))
     try:
         data, addr = s.recvfrom(3)
         send_paks()
     except socket.timeout:
         start_net(num_pak)
 
+
 def send_paks():
     count = 0
     for pak in arr:
-        if ack_arr[count] == False:
-            s.sendto(bytes(pak), (ip, port))
+        if not ack_arr[count]:
+            s.sendto(pak, (ip, port))
             time.sleep(0.1)
         count += 1
     ack()
 
+
 def ack():
     try:
         while True:
-            data, addr = s.recvfrom(100)
-            pck_num = str(data)[-3:len(str(data))]
+            data_ack, addr = s.recvfrom(100)
+            pck_num = str(data_ack)[-3:len(str(data_ack))]
             ack_arr[int(pck_num)] = True
     except:
         if not finish():
             send_paks()
+
 
 def finish():
     for b in ack_arr:
@@ -56,5 +60,5 @@ def finish():
     return True
 
 
-start_net(len(arr))
+start_net((len(arr)))
 s.close()
--- server.py
+++ server.py
@@ -1,9 +1,26 @@
 import socket
+import sys
 
+port = sys.argv[1]
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', 12345))
+s.bind(('', int(port)))
+num_pak, addr = s.recvfrom(3)
+s.sendto(num_pak, addr)
+arr_data = ['0'] * int((num_pak).decode('utf8'))
+arr_bool = [False for i in range(int(num_pak))]
+count = 0
+print("check")
+while count != int(num_pak):
+    data, addr = s.recvfrom(100)
+    if data == num_pak:
+        s.sendto(num_pak, addr)
+    else:
+        pkn_num = str(data)[-3:len(str(data))]
+        if not arr_bool[int(pkn_num)]:
+            arr_data[int(pkn_num)] = str(data)
+            arr_bool[int(pkn_num)] = True
+            count += 1
+        s.sendto(data, addr)
 
-while True:
-    data, addr = s.recvfrom(1024)
-    print(str(data), addr)
-    s.sendto(data.upper(), addr)
+for chunk in arr_data:
+    print(chunk)
