d0db9c08cef322eff9236da15a84cb78a7becb1a
benle <benlevi7770528@gmail.com>
2021-10-25 19:37:59 -0700

    server

--- server.py
+++ server.py
@@ -1,26 +1,30 @@
 import socket
 import sys
 
-port = sys.argv[1]
+port = int(sys.argv[1])
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', int(port)))
+s.bind('', port)
+
 num_pak, addr = s.recvfrom(3)
 s.sendto(num_pak, addr)
-arr_data = ['0'] * int((num_pak).decode('utf8'))
-arr_bool = [False for i in range(int(num_pak))]
+num_pak_int = int.from_bytes(num_pak, 'little')
+arr_data = [b'0'] * num_pak_int
+arr_bool = [False for i in range(num_pak_int)]
 count = 0
-print("check")
-while count != int(num_pak):
+while count != num_pak_int:
     data, addr = s.recvfrom(100)
     if data == num_pak:
         s.sendto(num_pak, addr)
     else:
-        pkn_num = str(data)[-3:len(str(data))]
-        if not arr_bool[int(pkn_num)]:
-            arr_data[int(pkn_num)] = str(data)
-            arr_bool[int(pkn_num)] = True
+        pkn_num = int.from_bytes(data[-3:len(data)], 'little')
+        print(pkn_num)
+        if not arr_bool[pkn_num]:
+            arr_data[pkn_num] = data
+            arr_bool[pkn_num] = True
             count += 1
         s.sendto(data, addr)
 
 for chunk in arr_data:
-    print(chunk)
+    print(chunk.decode('utf8'))
+
+s.close()
