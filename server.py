56cfc871f872629957bf3b9fb8623e2f9d6c314b
benle <benlevi7770528@gmail.com>
2021-10-25 20:30:32 -0700

    server

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import sys
 
 port = int(sys.argv[1])
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind('', port)
+s.bind(('', port))
 
 num_pak, addr = s.recvfrom(3)
 s.sendto(num_pak, addr)
@@ -11,7 +11,16 @@ num_pak_int = int.from_bytes(num_pak, 'little')
 arr_data = [b'0'] * num_pak_int
 arr_bool = [False for i in range(num_pak_int)]
 count = 0
-while count != num_pak_int:
+
+
+def print_data():
+    for chunk in arr_data:
+        print(chunk.decode('utf8'))
+
+
+while True:
+    if count == num_pak_int:
+        print_data()
     data, addr = s.recvfrom(100)
     if data == num_pak:
         s.sendto(num_pak, addr)
@@ -21,10 +30,7 @@ while count != num_pak_int:
         if not arr_bool[pkn_num]:
             arr_data[pkn_num] = data
             arr_bool[pkn_num] = True
-            count += 1
+            if count <= num_pak_int:
+                count += 1
         s.sendto(data, addr)
 
-for chunk in arr_data:
-    print(chunk.decode('utf8'))
-
-s.close()
