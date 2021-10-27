146ca8a2b81be4a50f848df5fed573c04ba45a9d
itay <etelis2019@gmail.com>
2021-10-27 11:38:33 +0300

    Added CONSTS and variables, added methods for receiving data and SYN, FIN methods in order to initialize and finish connection, client will close now when finishing connection while server will wait for additional connection.
    - Server's while loop logic was bit changed in order to accept new connections after finishing.
    - FIN methods were added in order to finish the connection between client and server.
    -SYN methods were added in order to start connection.

--- client.py
+++ client.py
@@ -2,38 +2,51 @@ import socket
 import sys
 import time
 
-ip = sys.argv[1]
-port = int(sys.argv[2])
-file_name = sys.argv[3]
+IP_ADDR = sys.argv[1]
+PORT = int(sys.argv[2])
+FILE_NAME = sys.argv[3]
+DEFAULT_TIMEOUT = 12
+FIN_MSG = b'FIN'
+SYN_MSG = b'SYN'
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.settimeout(12)
-
-text = open(file_name, "rb")
-data = text.read()
-arr = [data[i:i + 97] for i in range(0, len(data), 97)]
-for i in range(0, len(arr)):
-    arr[i] += i.to_bytes(3, 'little')
-
-ack_arr = [False for i in range(len(arr))]
+s.settimeout(DEFAULT_TIMEOUT)
+data_arr = [b'0']
+ack_arr = [False]
+arr_size_bytes = b'0'
 
 
-def start_net(num_pak):
-    s.sendto(num_pak, (ip, port))
+def initialize():
+    global data_arr, ack_arr, arr_size_bytes
+    text = open(FILE_NAME, "rb")
+    data = text.read()
+    data_arr = [(data[i:i + 97] + int((i / 97)).to_bytes(3, 'little')) for i in range(0, len(data), 97)]
+    arr_size_bytes = (len(data_arr)).to_bytes(3, 'little')
+    ack_arr = [False for i in range(0, len(data_arr), 1)]
+    text.close()
+
+
+def syn():
+    syn_msg = SYN_MSG + arr_size_bytes
+    s.sendto(syn_msg, (IP_ADDR, PORT))
     try:
-        get_num, addr = s.recvfrom(3)
-        send_paks()
+        get_syn, addr = s.recvfrom(100)
+        if get_syn != syn_msg:
+            syn()
+        else:
+            sent_pkgs()
     except socket.timeout:
-        start_net(num_pak)
+        syn()
 
 
-def send_paks():
+def sent_pkgs():
     count = 0
-    for pak in arr:
+    for count, pkg in enumerate(data_arr):
+        print("count = " + str(count))
         if not ack_arr[count]:
-            s.sendto(pak, (ip, port))
+            s.sendto(pkg, (IP_ADDR, PORT))
             time.sleep(0.1)
-        count += 1
     ack()
 
 
@@ -41,11 +54,30 @@ def ack():
     try:
         while True:
             data_ack, addr = s.recvfrom(100)
-            pck_num = int.from_bytes(data_ack[-3:len(data_ack)], 'little')
-            ack_arr[pck_num] = True
-    except:
+            if data_ack == FIN_MSG:
+                fin()
+            pkg_num = int.from_bytes(data_ack[-3:len(data_ack)], 'little')
+            print("got ack for:" + str(pkg_num))
+            ack_arr[pkg_num] = True
+
+    except socket.timeout:
         if not finish():
-            send_paks()
+            sent_pkgs()
+
+
+def fin():
+    try:
+        print("Waiting for server to return ack")
+        s.sendto(FIN_MSG, (IP_ADDR, PORT))
+        fin_ack, addr = s.recvfrom(100)
+        if fin_ack != FIN_MSG:
+            fin()
+        else:
+            print("Received bye bye!")
+            s.close()
+            sys.exit()
+    except socket.timeout:
+        fin()
 
 
 def finish():
@@ -55,5 +87,5 @@ def finish():
     return True
 
 
-start_net((len(arr)).to_bytes(3, 'little'))
-s.close()
+initialize()
+syn()
--- server.py
+++ server.py
@@ -1,37 +1,109 @@
 import socket
 import sys
 
-port = int(sys.argv[1])
+# Base variables and consts.
+PORT = int(sys.argv[1])
+FIN_MSG = b'FIN'
+SYN_MSG = b'SYN'
+SYN_FLG = False
+buffer_size = 0
+bool_arr = [False]
+data_arr = []
+iteration_counter = 1
+
+# UDP socket initialisation
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('', port))
-
-num_pak, addr = s.recvfrom(3)
-s.sendto(num_pak, addr)
-num_pak_int = int.from_bytes(num_pak, 'little')
-arr_data = [b'0'] * num_pak_int
-arr_bool = [False for i in range(num_pak_int)]
-count = 0
+s.bind(('', PORT))
 
 
+# function will receive packages and insert true to the boolean array if value was not received before.
+def recv_pkg(pkg):
+    pkg_num = int.from_bytes(pkg[-3:len(pkg)], 'little')
+    s.sendto(pkg, client)
+    if not bool_arr[pkg_num]:
+        print("Got package: " + str(pkg_num))
+        data_arr[pkg_num] = data
+        bool_arr[pkg_num] = True
+        return 1
+    return 0
+
+
+# syn function will resemble the sync method used by the TCP protocol to initialise the connection.
+def syn(syn_msg, addr):
+    s.sendto(syn_msg, addr)
+    syn_msg, addr = s.recvfrom(100)
+    # if the message received is not the first package, than resend the syn ack confirmation.
+    if (int.from_bytes(syn_msg[-3:len(syn_msg)], 'little')) != 0:
+        print("Wanted pkg 000, but got: " + str(int.from_bytes(syn_msg[-3:len(syn_msg)], 'little')))
+        syn(syn_msg, client)
+    # otherwise, return the 000 package and move on to accepting packages.
+    else:
+        return syn_msg
+
+
+# This function will print the data when all received.
 def print_data():
-    for chunk in arr_data:
+    print("Printing stage")
+    for chunk in data_arr:
         print(chunk.decode('utf8'))
 
 
-while True:
-    if count == num_pak_int:
-        print_data()
-    data, addr = s.recvfrom(100)
-    if data == num_pak:
-        s.sendto(num_pak, addr)
-    else:
-        pkn_num = int.from_bytes(data[-3:len(data)], 'little')
-        # print(pkn_num)
-        if not arr_bool[pkn_num]:
-            arr_data[pkn_num] = data
-            arr_bool[pkn_num] = True
-            if count <= num_pak_int:
-                count += 1
-        # that's why the client will stop at the end.
-        s.sendto(data, addr)
+# This function will initialize all variables before each socket connected.
+def initialize():
+    print("Finished And initialized.")
+    global buffer_size, bool_arr, data_arr, SYN_FLG, iteration_counter
+    buffer_size = 0
+    bool_arr = [False]
+    data_arr = [b'0']
+    SYN_FLG = False
+    iteration_counter = 1
+    s.settimeout(None)
 
+
+# This method will resemble the fin method used by the TCP protocol
+# by the end of the connection will signal the server to stop.
+def fin(client):
+    # timeout is set in-order to make sure no future messages are sent after fin
+    s.settimeout(15)
+    s.sendto(FIN_MSG, client)
+    try:
+        fin_ack, client = s.recvfrom(100)
+        fin(client)
+
+    # if no message received within timeline move to initialize all variables.
+    except socket.timeout:
+        initialize()
+
+
+# The following while loop will operate as the server itself, accepting connections and printing data.
+while True:
+    data, client = s.recvfrom(100)
+    # if SYN_FLG off - meaning no sync was made with client yet, and message received is sync message,
+    # synchronize connection.
+    if not SYN_FLG and data[0:3] == SYN_MSG:
+        # buffer_size is the amount of packages we are expecting.
+        buffer_size = int.from_bytes(data[-3:len(data)], 'little')
+        data = syn(data, client)
+        print("Buffer size is:" + str(buffer_size))
+        SYN_FLG = True
+        # Initialize arrays according to size received.
+        bool_arr = [False for i in range(0, buffer_size, 1)]
+        data_arr = [b'0'] * buffer_size
+        recv_pkg(data)
+
+    # if FIN message received after connection has been close
+    # meaning the client did not receive the message and we should re send it
+    elif data == FIN_MSG:
+        fin(client)
+
+    # if sync with client made successfully move to accept all packages from client.
+    if buffer_size > 0:
+        # The next while loop will iterate until all packages received, meaning bool_arr is all true.
+        while iteration_counter < buffer_size:
+            data, client = s.recvfrom(100)
+            print("iteration_counter = " + str(iteration_counter))
+            # if package received was not received before rev_pkg will return 1, otherwise expect 0.
+            iteration_counter += recv_pkg(data)
+        # all packages has been successfully accepted, we will print the data and finnish connection with client.
+        print_data()
+        fin(client)
